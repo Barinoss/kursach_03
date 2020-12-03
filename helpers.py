50ed9fd17a05f4e87ee4eaabd21719661689b917
Marko <markobarin@gmail.ocm>
2020-12-03 02:24:19 +0200

    init

--- helpers.py
+++ helpers.py
@@ -9,15 +9,20 @@ def draw_fft_test(freqs, threshold=50, noise=2):
           freqs[i] = int(freqs[i])
      plt.rcParams['figure.figsize'] = [16, 12]
      plt.rcParams.update({'font.size': 18})
-     dt = 0.5/(max(freqs)+max(freqs)*0.05)/500  #/50  # Разобратсья с частотой дискретизации, диапазоном частот в ффт !!!!!!!!!!!!!!!!!!
+     mult = int(max(freqs)/2)
+     if mult < 1:
+          mult = 1
+     dt = 0.5/(max(freqs)+max(freqs)*0.05)/mult #/50  # Разобратсья с частотой дискретизации, диапазоном частот в ффт !!!!!!!!!!!!!!!!!!
      t = np.arange(0, 1, dt)
      f = np.sin(2 * np.pi * freqs[0] * t)
      for i in range(1, len(freqs)):
           f += np.sin(2 * np.pi * freqs[i] * t)
 
-     f_clean = f
 
      f += noise*np.random.randn(len(t))
+     a = f.tolist()
+
+     f = np.array(a)
 
      n = len(t)
      fhat = np.fft.fft(f, n)
@@ -27,39 +32,86 @@ def draw_fft_test(freqs, threshold=50, noise=2):
 
 
      indices = PSD > threshold
-
      PSDclean = PSD * indices
-
      fhat = indices * fhat
      ffilt = np.fft.ifft(fhat)
 
      fig,axs = plt.subplots(4,1)
 
      plt.sca(axs[0])
-     plt.plot(t, f, label='Сигнал с шумами', color='r')
+     plt.plot(t, f, label='Сигнал з шумами', color='r')
      plt.xlim(t[0], t[-1])
      plt.legend()
 
      plt.sca(axs[1])
-     plt.plot(t, ffilt, label='Отфильтрованный сигнал', color='k')
+     plt.plot(t, ffilt, label='Відфільтрований сигнал', color='k')
      plt.xlim(t[0], t[-1])
      plt.legend()
 
      plt.sca(axs[2])
-     plt.plot(freq[L], PSD[L], color='c', label="FFT")
-     plt.xlim(freq[L[0]],freq[L[-1]])
+     plt.plot(freq[L], PSD[L], color='c', label="ШПФ")
+     plt.xlim(freq[L[0]],freq[L[int(len(L)/mult)-1]])
      plt.legend()
 
      plt.sca(axs[3])
-     plt.plot(freq[L], PSD[L], color='c', label="Шумы")
-     plt.plot(freq[L], PSDclean[L], color='k', label='Полезные частоты')
-     plt.xlim(freq[L[0]],freq[L[-1]])
+     plt.plot(freq[L], PSD[L], color='c', label="Шуми")
+     plt.plot(freq[L], PSDclean[L], color='k', label='Корисні частоти')
+     plt.xlim(freq[L[0]],freq[L[int(len(L)/mult)-1]])
      plt.legend()
      name = str(freqs).replace(' ', '')
      name = str(name+str(threshold)+"+"+str(noise))
      plt.savefig('static/{}.png'.format(name))
-
+     #print(f.tolist())
      return name
+
+
+def draw_fft(freqs, threshold=50):
+     plt.rcParams['figure.figsize'] = [16, 12]
+     plt.rcParams.update({'font.size': 18})
+     dt = 1/len(freqs)
+     t = np.arange(0, 1, dt)
+
+
+     a = freqs
+     f = np.array(a)
+     n = len(t)
+     fhat = np.fft.fft(f, n)
+     PSD = fhat * np.conj(fhat) / n
+     freq = (1/(dt*n)) * np.arange(n)
+     L = np.arange(1,np.floor(n/2), dtype='int')
+
+
+     indices = PSD > threshold
+     PSDclean = PSD * indices
+     psdlist = PSDclean.tolist()
+     fhat = indices * fhat
+     ffilt = np.fft.ifft(fhat)
+
+     fig,axs = plt.subplots(4,1)
+
+     plt.sca(axs[0])
+     plt.plot(t[:len(f)], f, label='Сигнал з шумами', color='r')
+     plt.xlim(t[0], t[-1])
+     plt.legend()
+
+     plt.sca(axs[1])
+     plt.plot(t, ffilt, label='Відфільтрований сигнал', color='k')
+     plt.xlim(t[0], t[-1])
+     plt.legend()
+
+     plt.sca(axs[2])
+     plt.plot(freq[L], PSD[L], color='c', label="ШПФ")
+     plt.xlim(freq[L[0]],freq[L[-1]])
+     plt.legend()
+
+     plt.sca(axs[3])
+     plt.plot(freq[L], PSD[L], color='c', label="Шуми")
+     plt.plot(freq[L], PSDclean[L], color='k', label='Корисні частоти')
+     plt.xlim(freq[L[0]],freq[L[-1]])
+     plt.legend()
+     name = str(freqs[0])
+     plt.savefig('static/{}.png'.format(name))
+     return name, ffilt.tolist()
 
 
 if __name__ == '__main__':
--- myproject.py
+++ myproject.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, request, session
 from flask_session import Session
-from helpers import draw_fft_test
-import json
-from datetime import datetime
+from helpers import draw_fft_test, draw_fft
+
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
+@app.route("/kursach", methods=["GET", "POST"])
+def kursach():
     if session.get("freq") is None:
         session["freq"] = ''
     if request.method == "POST":
@@ -21,16 +20,38 @@ def index():
         if session["noise"] == None:
             session["noise"] = 2
     if session["freq"]:
-        all_freqs = list(map(float, session["freq"].split(',')))
+        try:
+            all_freqs = list(map(float, session["freq"].split(',')))
+        except:
+            all_freqs = [1, 2, 3]
         image_name = draw_fft_test(all_freqs, session["threshold"], session["noise"])+'.png'
     else:
         image_name = 'none'
-    return render_template("index.html", freq=session["freq"], image_name=image_name)
+    return render_template("kursach.html", freq=session["freq"], image_name=image_name)
 
 
-@app.route("/kursach")
-def table():
-    return render_template('kursach.html')
+@app.route("/kursach_2", methods=["GET", "POST"])
+def kursach_2():
+    if session.get("signal") is None:
+        session["signal"] = ''
+    if request.method == "POST":
+        session["signal"] = request.form.get("signal")
+        session["threshold"] = int(request.form.get("threshold"))
+        if session["threshold"] == None:
+            session["threshold"] = 50
+    try:
+        a = str(session["signal"])
+        b = a[1:-1].replace(' ', '').split(',')
+        all_freqs = list(map(float, b))
+    except:
+        all_freqs = [1, 2, 3]
+    image_name, signal2 = draw_fft(all_freqs, session["threshold"])
+    image_name += '.png'
+    return render_template("kursach_2.html", image_name=image_name, signal2=signal2)
+
+@app.route("/")
+def index():
+    return render_template("layout.html")
 
 
 if __name__ == '__main__':
